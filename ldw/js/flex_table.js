671d31ebecf05c3d2d498a3ffd188b57f8946d4f
lamb0693 <mar189@naver.com>
2023-07-24 21:13:28 +0900

    add popup wnd

--- ldw/js/flex_table.js
+++ ldw/js/flex_table.js
@@ -3,5 +3,22 @@ const onModify = (event) =>{
 }
 
 const onDelete = (event) =>{
-    console.log("delete : " + event.target.id)
+    const modal_dlg_bg = document.getElementById("modal_dlg_bg");
+    const modal_dlg_form = document.getElementById("modal_dlg_form")
+    let width = window.innerWidth
+    modal_dlg_form.style.left = (width/2 - 150) + 'px'
+    modal_dlg_bg.style.display = "block"
+     console.log("delete : " + event.target.id)
+}
+
+const on_modal_ok = (event) => {
+    const modal_dlg_bg = document.getElementById("modal_dlg_bg");
+    modal_dlg_bg.style.display = "none"
+    console.log("on_modal_ok : " + event.target.id)
+}
+
+const on_modal_cancel = (event) => {
+    const modal_dlg_bg = document.getElementById("modal_dlg_bg");
+    modal_dlg_bg.style.display = "none"
+    console.log("on_modal_cancel : " + event.target.id)
 }
