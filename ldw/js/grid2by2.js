4d86ba6c3d3154937cafee42030b7b45e6c21b94
lamb0693 <mar189@naver.com>
2023-07-27 07:05:22 +0900

    modify some

--- ldw/js/grid2by2.js
+++ ldw/js/grid2by2.js
@@ -1,27 +1,20 @@
-const onModify = (event) =>{
-    event.target.preventDefault()
-    console.log("modify : " +event.target.id)
-}
-
-const onDelete = (event) =>{
+const onLogin = (event) =>{
     event.target.preventDefault()
     const modal_dlg_bg = document.getElementById("modal_dlg_bg");
     const modal_dlg_form = document.getElementById("modal_dlg_form")
     let width = window.innerWidth
     modal_dlg_form.style.left = (width/2 - 150) + 'px'
     modal_dlg_bg.style.display = "block"
-     console.log("delete : " + event.target.id)
+     console.log("onLogin : " + event.target.id)
 }
 
 const on_modal_ok = (event) => {
-    event.target.preventDefault()
     const modal_dlg_bg = document.getElementById("modal_dlg_bg");
     modal_dlg_bg.style.display = "none"
     console.log("on_modal_ok : " + event.target.id)
 }
 
 const on_modal_cancel = (event) => {
-    event.target.preventDefault()
     const modal_dlg_bg = document.getElementById("modal_dlg_bg");
     modal_dlg_bg.style.display = "none"
     console.log("on_modal_cancel : " + event.target.id)
